3f1025f47c3a788d90d0e080d952a87313d93c6b
jashanpreet-singh-99 <singh872@uwindsor.ca>
2024-07-15 10:33:35 -0400

    Added Select options to Provice in quick search

--- src/app/components/shared/quick-search/quick-search.component.ts
+++ src/app/components/shared/quick-search/quick-search.component.ts
@@ -4,6 +4,11 @@ import { ListingSearch } from './models/listing-search';
 import { CommonModule } from '@angular/common';
 import { QuickSearchMode } from './enums/quick_search_mode';
 
+interface ProvinceData {
+  name: string,
+  value: string
+}
+
 @Component({
   selector: 'quick-search',
   standalone: true,
@@ -20,6 +25,61 @@ export class QuickSearchComponent implements OnInit {
   @Input() disableModeControls: boolean = false;
   @Input() defaultValue: ListingSearch | undefined;
   @Output() searchResult: EventEmitter<ListingSearch> = new EventEmitter<ListingSearch>();
+
+  provinces: ProvinceData[] = [
+    {
+      name: 'Alberta',
+      value: 'AB'
+    },
+    {
+      name: 'British Columbia',
+      value: 'BC'
+    },
+    {
+      name: 'Manitoba',
+      value: 'MB'
+    },
+    {
+      name: 'New Brunswick',
+      value: 'NB'
+    },
+    {
+      name: 'Newfoundland and Labrador',
+      value: 'NL'
+    },
+    {
+      name: 'Nova Scotia',
+      value: 'NS'
+    },
+    {
+      name: 'Northwest Territories',
+      value: 'NT'
+    },
+    {
+      name: 'Nunavut',
+      value: 'NU'
+    },
+    {
+      name: 'Ontario',
+      value: 'ON'
+    },
+    {
+      name: 'Prince Edward Island',
+      value: 'PE'
+    },
+    {
+      name: 'Quebec',
+      value: 'QC'
+    },
+    {
+      name: 'Saskatchewan',
+      value: 'SK'
+    },
+    {
+      name: 'Yukon',
+      value: 'YT'
+    }
+  ];
 
   searchForm: FormGroup;
 
