9627614abc760b665602f93a357e278366e76a02
jashanpreet-singh-99 <singh872@uwindsor.ca>
2024-07-15 10:42:52 -0400

    Added animation on page reload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,20 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from "./components/shared/nav-bar/nav-bar.component";
 import { FooterSectionComponent } from "./components/www/footer-section/footer-section.component";
+import { fadeAnimation } from './animations/animations';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, NavBarComponent, FooterSectionComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  animations: [fadeAnimation]
 })
 export class AppComponent {
   title = 'estate-360';
+
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
+  }
 }
