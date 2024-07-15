e0536925dc6f6de315f49917934424ac8145b6f8
jashanpreet-singh-99 <singh872@uwindsor.ca>
2024-07-15 04:45:44 -0400

    Added footer section and remove pagination

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from "./components/shared/nav-bar/nav-bar.component";
+import { FooterSectionComponent } from "./components/www/footer-section/footer-section.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent],
+  imports: [RouterOutlet, NavBarComponent, FooterSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
